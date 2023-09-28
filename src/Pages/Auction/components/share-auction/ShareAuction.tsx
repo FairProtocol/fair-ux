a11081f0fd72b07818907a55705e2c7be7f93ba1
kdash27599 <kevin27599@proton.me>
2023-09-28 19:50:18 +0530

    feat: Share modal

---
+++ src/Pages/Auction/components/share-auction/ShareAuction.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react'
+
+import { Button, TextField, Typography } from '@mui/material'
+import Facebook from 'src/assets/images/facebook_small.svg'
+import Telegram from 'src/assets/images/telegram_small.svg'
+import Twitter from 'src/assets/images/twitter_small.svg'
+
+import Wrapper from '../wrapper/Wrapper'
+import './ShareAuction.scss'
+
+const ShareAuction: React.FC = () => {
+  const [auctionUrl, setAuctionUrl] = useState('')
+  const [shareUrl, setShareUrl] = useState('')
+
+  useEffect(() => {
+    setAuctionUrl(window.location.href.replace('#topAnchor', ''))
+    // Since '#' and '&' needs to be encoded
+    setShareUrl(
+      window.location.href.replace('#', '%23').replace('&', '%26').replace('#topAnchor', ''),
+    )
+  }, [])
+
+  return (
+    <Wrapper className="share-auction">
+      <Typography className="share-auction_header">Share this Auction</Typography>
+      <div className="share-auction_socials">
+        <a
+          className="share-auction_socials_image"
+          href={`https://www.facebook.com/sharer/sharer.php?u=${shareUrl}`}
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img src={Facebook} />
+        </a>
+        <a
+          className="share-auction_socials_other-image"
+          href={`https://t.me/share/url?url=${shareUrl}`}
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img src={Telegram} />
+        </a>
+        <a
+          className="share-auction_socials_other-image"
+          href={`https://twitter.com/intent/tweet?url=${shareUrl}`}
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img src={Twitter} />
+        </a>
+      </div>
+      <Typography className="share-auction_header">or copy link</Typography>
+      <TextField
+        InputProps={{
+          endAdornment: (
+            <Button onClick={() => navigator.clipboard.writeText(auctionUrl)}>Copy</Button>
+          ),
+          readOnly: true,
+          className: 'share-auction_copy',
+        }}
+        fullWidth
+        value={auctionUrl}
+      />
+    </Wrapper>
+  )
+}
+
+export default ShareAuction
--- src/Pages/Auction/components/tab_selection/data.ts
+++ src/Pages/Auction/components/tab_selection/data.ts
@@ -1,5 +1,6 @@
 import { ReactComponent as AuctionHammer } from 'src/assets/images/auction_hammer.svg'
 import { ReactComponent as Info } from 'src/assets/images/info.svg'
+import { ReactComponent as Share } from 'src/assets/images/share.svg'
 
 export const tabs: { name: string; Icon: any }[] = [
   {
@@ -9,5 +10,9 @@ export const tabs: { name: string; Icon: any }[] = [
   {
     name: 'Information',
     Icon: Info,
+  },
+  {
+    name: 'Share',
+    Icon: Share,
   },
 ]
